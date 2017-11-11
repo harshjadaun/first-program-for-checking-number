6d39e5dbbc7ca2f8dda81c1d632d9b3f3d65562f
harshjadaun <30822394+harshjadaun@users.noreply.github.com>
2017-11-11 15:22:17 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class A(){
+ public static void main(String args[]){
+  Scanner sc=new Scanner(System.in);
+  char ch;
+  ch=sc.next();
+  if(ch.alphabet()){
+   System.out.println("alphabet");
+  }
+  else{
+    System.out.println("not alphabet");
+  }  
+ }
+} 
