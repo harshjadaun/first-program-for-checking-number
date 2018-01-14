74cf43c9b707576bf17bd2c15dd2f7bd2a984df2
harshjadaun <30822394+harshjadaun@users.noreply.github.com>
2018-01-14 04:40:49 -0800

    Update alphabet.java

--- alphabet.java
+++ alphabet.java
@@ -1,14 +1,13 @@
 import java.util.*;
 public class A(){
  public static void main(String args[]){
-  Scanner sc=new Scanner(System.in);
   char ch;
-  ch=sc.next();
-  if(ch.alphabet()){
+  ch=(char)System.in.read();
+  if((ch>='A' && ch<='Z')||(ch>='a' && ch<='z')){
    System.out.println("alphabet");
   }
   else{
-    System.out.println("not alphabet");
-  }  
- }
+   System.out.println("not alphabet");
+  } 
+	}
 } 
