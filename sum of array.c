8e2a00b1113507e978123b2769172cda21040fe0
harshjadaun <30822394+harshjadaun@users.noreply.github.com>
2018-01-14 08:08:02 -0800

    Create sum of array.c

---
+++ sum of array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main(void) {
+	// your code goes here
+	int i,N,K,arr[100],sum=0;
+	scanf("%d%d",&N,&K);
+	for(i=0;i<N;i++){
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<K;i++){
+		sum+=arr[i];
+	}
+	printf("%d",sum);
+	return 0;
+}
